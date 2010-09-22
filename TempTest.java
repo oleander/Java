8d3062453d4bcdbf4924162a8a2e661ee41d61e4
Jesper Josefsson <jesper.josefsson@gmail.com>
2010-09-23 00:22:07 +0200

    1

---
+++ TempTest.java
@@ -0,0 +1,12 @@
+class TempTest {
+  
+  public static void main(String[] args) {
+    Rational r = new Rational(7,14);
+    Rational r1 = new Rational(1,2);
+    
+    if (r.equals(r1)) {
+      System.out.println("Ja!");
+    }
+  }
+  
+}
